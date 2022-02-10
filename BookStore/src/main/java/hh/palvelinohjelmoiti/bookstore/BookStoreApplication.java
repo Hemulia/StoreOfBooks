8c6c876ff8c99a77c491c726d4eb7d01dc28e7aa
Emilia Mäkelä <bgr887@myy.haaga-helia.fi>
2022-02-10 19:38:36 +0200

    Päivitys

--- BookStore/src/main/java/hh/palvelinohjelmoiti/bookstore/BookStoreApplication.java
+++ BookStore/src/main/java/hh/palvelinohjelmoiti/bookstore/BookStoreApplication.java
@@ -1,12 +1,30 @@
 package hh.palvelinohjelmoiti.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.palvelinohjelmoiti.bookstore.domain.Book;
+import hh.palvelinohjelmoiti.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			Book b1 = new Book("Hyvän Historia", "Rutger Bergman", 2019, "978-952-300-676-8", 15.99);
+			Book b2 = new Book("The Tipping Point", "Malcom Gladwell", 2013, "978-0-349-11346-3", 12.99);
+			Book b3 = new Book("Luonnon Voimat", "Jane Harper", 2016, "978-952-442-741-6", 12.90);
+
+			repository.save(b1);
+			repository.save(b2);
+			repository.save(b3);
+		};
 	}
 }
--- BookStore/src/main/java/hh/palvelinohjelmoiti/bookstore/domain/Book.java
+++ BookStore/src/main/java/hh/palvelinohjelmoiti/bookstore/domain/Book.java
@@ -1,6 +1,16 @@
 package hh.palvelinohjelmoiti.bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+
+	private long id;
 
 	private String title;
 
@@ -26,7 +36,6 @@ public class Book {
 	public Book(String title, String author, int year, String isbn, double price) {
 
 		super();
-
 		this.title = title;
 		this.author = author;
 		this.year = year;
@@ -34,56 +43,60 @@ public class Book {
 		this.price = price;
 	}
 
-	// sets
+	// toString
 
-	public void setTitle(String title) {
-		this.title = title;
+	public long getId() {
+		return id;
 	}
 
-	public void setAuthor(String author) {
-		this.author = author;
+	public void setId(long id) {
+		this.id = id;
 	}
-
-	public void setYear(int year) {
-		this.year = year;
-	}
-
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	// gets
 
 	public String getTitle() {
 		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
 	public String getAuthor() {
 		return author;
 	}
 
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
 	public int getYear() {
 		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
 	}
 
 	public String getIsbn() {
 		return isbn;
 	}
 
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
 	public double getPrice() {
 		return price;
 	}
 
-	// toString
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
 	@Override
 	public String toString() {
-		return "Book [title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price
-				+ "]";
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+				+ ", price=" + price + "]";
 	}
 
 }
