aeeb996b3b041527dc4f5b5d3d7f58fae3b9e421
Emilia Mäkelä <bgr887@myy.haaga-helia.fi>
2022-02-16 19:42:04 +0200

    d

--- BookStore/src/main/java/hh/palvelinohjelmoiti/bookstore/domain/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package hh.palvelinohjelmoiti.bookstore.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-
-}
